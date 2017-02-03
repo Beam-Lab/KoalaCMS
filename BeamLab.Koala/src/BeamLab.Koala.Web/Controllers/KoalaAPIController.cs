bed8f5ebd10907d8d30da294ef0287c71368ea4d
Emanuele B. <bartolesiemanuele@hotmail.com>
2017-02-03 11:34:27 +0100

    Swagger

--- BeamLab.Koala/src/BeamLab.Koala.Web/Controllers/KoalaAPIController.cs
+++ BeamLab.Koala/src/BeamLab.Koala.Web/Controllers/KoalaAPIController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BeamLab.Koala.Web.Repository;
+using BeamLab.Koala.Web.Models;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -24,25 +25,43 @@ namespace BeamLab.Koala.Web.Controllers
         /// </summary>
         /// <param name="id"></param>
         [HttpGet]
+        [Route("AllArticles")]
         [ApiExplorerSettings(GroupName = "v1")]
-        public IEnumerable<string> Get()
+        public IEnumerable<Article> AllArticles()
         {
-            return new string[] { "value1", "value2" };
+            var articles = _repository.GetAllArticles();
+
+            return articles;
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
+        /// <summary>
+        /// Returns an article.
+        /// </summary>
+        /// <param name="id">the article id</param>
+        [HttpGet("ArticleDetail/{id}")]
         [ApiExplorerSettings(GroupName = "v1")]
-        public string Get(int id)
+        public IActionResult Article(int id)
         {
-            return "value";
+            return Ok(_repository.GetArticle(id));
         }
 
-        // POST api/values
-        [HttpPost]
+        /// <summary>
+        /// How many articles?.
+        /// </summary>
+        /// <param name="count">the article count</param>
+        [HttpGet]
+        [Route("LatestArticles")]
         [ApiExplorerSettings(GroupName = "v1")]
-        public void Post([FromBody]string value)
+        public IActionResult LatestArticle()
         {
+            //if (count == 0)
+            //{
+            //    return BadRequest();
+            //}
+
+            var articles = _repository.GetLatestArticles();
+
+            return Ok(articles);
         }
     }
 }
